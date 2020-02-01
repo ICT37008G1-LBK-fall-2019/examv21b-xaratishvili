c69aa0b24f44dc38a7c3c078b24f2a024d78fa3b
xaratishvili <56753610+xaratishvili@users.noreply.github.com>
2020-02-01 12:42:25 +0400

    exam fix

--- exc3/scripts.js
+++ exc3/scripts.js
@@ -3,15 +3,15 @@ var meore="ate145Bgg"
 var regex=/^[a-zA-Z]{3}[1,3,5,8]{5}[A,B]{2}$/;
 if (pirveli.match(regex))
 {
-    console.log("match");
+    console.log("right");
 }
 else {
-    console.log("no match");
+    console.log("wrong");
 }
 if(meore.match(regex))
 {
-    console.log("match");
+    console.log("right");
 }
 else{
-    console.log("no match");
+    console.log("wrong");
 }
